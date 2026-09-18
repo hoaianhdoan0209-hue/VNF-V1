6cdfa76b2889b4d4d0c75c7cb261b9a99727a985
hoaianhdoan0209-hue <hoaianhdoan0209@gmail.com>
2026-09-18 14:46:45 +0700

    Add joint constraints and linked quadruped cat rig

---
+++ app/src/main/java/com/aicharacter/v3/CatRigState.java
@@ -0,0 +1,8 @@
+package com.aicharacter.v3;
+import org.json.JSONObject;
+/** One linked quadruped body: fore/hind contacts share one trunk and center of mass. */
+public final class CatRigState{
+ public double gaitPhase,trunkLean,spineFlex,frontLeftContact=1,frontRightContact=1,hindLeftContact=1,hindRightContact=1,frontLoad=.5,hindLoad=.5;
+ public JSONObject toJson(){JSONObject j=new JSONObject();try{j.put("gaitPhase",gaitPhase);j.put("trunkLean",trunkLean);j.put("spineFlex",spineFlex);j.put("frontLeftContact",frontLeftContact);j.put("frontRightContact",frontRightContact);j.put("hindLeftContact",hindLeftContact);j.put("hindRightContact",hindRightContact);j.put("frontLoad",frontLoad);j.put("hindLoad",hindLoad);}catch(Exception ignored){}return j;}
+ public static CatRigState fromJson(JSONObject j){CatRigState c=new CatRigState();if(j==null)return c;c.gaitPhase=j.optDouble("gaitPhase");c.trunkLean=j.optDouble("trunkLean");c.spineFlex=j.optDouble("spineFlex");c.frontLeftContact=j.optDouble("frontLeftContact",1);c.frontRightContact=j.optDouble("frontRightContact",1);c.hindLeftContact=j.optDouble("hindLeftContact",1);c.hindRightContact=j.optDouble("hindRightContact",1);c.frontLoad=j.optDouble("frontLoad",.5);c.hindLoad=j.optDouble("hindLoad",.5);return c;}
+}
