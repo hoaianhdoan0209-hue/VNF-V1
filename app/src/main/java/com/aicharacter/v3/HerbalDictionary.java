980bbddfd4648e0cca111ce8e9d7d1010672757d
hoaianhdoan0209-hue <hoaianhdoan0209@gmail.com>
2026-09-21 10:44:33 +0700

    Align fictional medicinal flora with VNF-native biome ids

--- app/src/main/java/com/aicharacter/v3/HerbalDictionary.java
+++ app/src/main/java/com/aicharacter/v3/HerbalDictionary.java
@@ -4,9 +4,9 @@ import java.util.*;
 public final class HerbalDictionary{
  private static final Map<String,HerbDefinition> ALL=new LinkedHashMap<>();
  static{
-  add(new HerbDefinition("mistleaf","phiến sương","humid_meadow_edge","những phiến mềm ánh bạc khép mở theo rung động của nền đất","DEW_FOLD",.12,.28,.05,.08,.10));
-  add(new HerbDefinition("blue_reed","lam ty","freshwater_lakeshore","những sợi thân lam rỗng phát nhịp mờ khi không khí ẩm lên cao","LUMEN_REST",.30,.06,.12,.16,.18));
-  add(new HerbDefinition("ember_moss","hồng mạch","humid_broadleaf_grove","mạng mô thấp màu tối có những điểm hồng co giãn như mạch sáng dưới bóng râm","ECHO_DRY",.10,.16,.26,.14,.15));
+  add(new HerbDefinition("mistleaf","phiến sương","silverfold_verge","những phiến mềm ánh bạc khép mở theo rung động của nền đất","DEW_FOLD",.12,.28,.05,.08,.10));
+  add(new HerbDefinition("blue_reed","lam ty","lumenmere_margin","những sợi thân lam rỗng phát nhịp mờ khi không khí ẩm lên cao","LUMEN_REST",.30,.06,.12,.16,.18));
+  add(new HerbDefinition("ember_moss","hồng mạch","veilroot_grove","mạng mô thấp màu tối có những điểm hồng co giãn như mạch sáng dưới bóng râm","ECHO_DRY",.10,.16,.26,.14,.15));
  }
  private HerbalDictionary(){}
  private static void add(HerbDefinition d){ALL.put(d.id,d);}
