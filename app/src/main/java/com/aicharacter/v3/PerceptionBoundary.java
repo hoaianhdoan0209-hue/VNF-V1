87e614b141ba3917a01337d0ea0c2bc16dd78844
hoaianhdoan0209-hue <hoaianhdoan0209@gmail.com>
2026-09-21 10:27:00 +0700

    Use live creature positions in Haru spatial perception

--- app/src/main/java/com/aicharacter/v3/PerceptionBoundary.java
+++ app/src/main/java/com/aicharacter/v3/PerceptionBoundary.java
@@ -8,7 +8,7 @@ public final class PerceptionBoundary {
     public static PerceivedObject perceive(WorldState s,WorldObject o){
         if(!HaruVisionEngine.canSee(s,o))return new PerceivedObject("unknown","không nhìn thấy rõ vật đó","ngoài nhận thức hiện tại",false);
         boolean familiar=false;for(MemoryEntry m:s.memories)if(o.id.equals(m.location)||m.tags.contains(o.id)||m.participants.contains(o.id)){familiar=true;break;}
-        String rel=o.x<s.haruX-70?"ở phía bên trái":o.x>s.haruX+70?"ở phía bên phải":"ở ngay gần đây";
+        float ox=HaruVisionEngine.actualX(s,o);String rel=ox<s.haruX-70?"ở phía bên trái":ox>s.haruX+70?"ở phía bên phải":"ở ngay gần đây";
         return new PerceivedObject(o.id,o.haruDescription,rel,familiar);
     }
     public static String haruPerception(WorldState s){int h=(int)(s.worldMinutes/60);String time=h<6?"đêm":h<11?"buổi sáng":h<17?"ban ngày":h<20?"chiều tối":"đêm";String weather="CLEAR".equals(s.environment.weather)?"trời quang":"CLOUDY".equals(s.environment.weather)?"trời nhiều mây":"trời đang mưa";WorldArea a=s.world.areaAt(s.haruX);return "Bây giờ là "+time+", "+weather+". Cô ấy đang ở "+(a==null?"một nơi khó xác định":a.haruName)+" và "+s.haruActivity+".";}
