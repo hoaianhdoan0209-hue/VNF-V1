e75d59dfee63bd5cfcb06913ab3925a639b006b2
hoaianhdoan0209-hue <hoaianhdoan0209@gmail.com>
2026-09-21 10:41:39 +0700

    Make VNF medicinal flora explicitly non-Earth fantasy species

--- app/src/main/java/com/aicharacter/v3/HerbalDictionary.java
+++ app/src/main/java/com/aicharacter/v3/HerbalDictionary.java
@@ -4,9 +4,9 @@ import java.util.*;
 public final class HerbalDictionary{
  private static final Map<String,HerbDefinition> ALL=new LinkedHashMap<>();
  static{
-  add(new HerbDefinition("mistleaf","lá sương bạc","humid_meadow_edge","lá nhỏ phủ ánh bạc, mọc sát cỏ ẩm","RINSE_CRUSH",.12,.28,.05,.08,.10));
-  add(new HerbDefinition("blue_reed","sậy lam","freshwater_lakeshore","thân sậy mảnh có viền xanh lam nhạt","WARM_STEEP",.30,.06,.12,.16,.18));
-  add(new HerbDefinition("ember_moss","rêu hồng ấm","humid_broadleaf_grove","mảng rêu sẫm có chấm hồng, thường ở gốc cây râm","DRY_CRUSH",.10,.16,.26,.14,.15));
+  add(new HerbDefinition("mistleaf","phiến sương","humid_meadow_edge","những phiến mềm ánh bạc khép mở theo rung động của nền đất","DEW_FOLD",.12,.28,.05,.08,.10));
+  add(new HerbDefinition("blue_reed","lam ty","freshwater_lakeshore","những sợi thân lam rỗng phát nhịp mờ khi không khí ẩm lên cao","LUMEN_REST",.30,.06,.12,.16,.18));
+  add(new HerbDefinition("ember_moss","hồng mạch","humid_broadleaf_grove","mạng mô thấp màu tối có những điểm hồng co giãn như mạch sáng dưới bóng râm","ECHO_DRY",.10,.16,.26,.14,.15));
  }
  private HerbalDictionary(){}
  private static void add(HerbDefinition d){ALL.put(d.id,d);}
