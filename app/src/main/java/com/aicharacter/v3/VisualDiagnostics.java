d9069c3fe7a5deae6f7d0dc9f6592f5a11ef8565
hoaianhdoan0209-hue <hoaianhdoan0209@gmail.com>
2026-09-21 13:51:11 +0700

    Expose Haru jump height and vertical camera follow diagnostics

--- app/src/main/java/com/aicharacter/v3/VisualDiagnostics.java
+++ app/src/main/java/com/aicharacter/v3/VisualDiagnostics.java
@@ -1,5 +1,5 @@
 package com.aicharacter.v3;
 public final class VisualDiagnostics{private VisualDiagnostics(){}
  public static String animation(WorldState s){GirlAnimationController.Visual v=GirlAnimationController.select(s);return"simulation="+s.haruActivity+"\nvisual="+v.state+"\nasset="+v.asset+"\nfacing="+(v.state==GirlAnimationController.State.WALK_LEFT?"LEFT":"RIGHT")+"\nframes="+v.frames+" fps="+v.fps+"\nreason="+v.reason;}
- public static String camera(WorldState s){CatCameraDirector.Frame f=CatCameraDirector.direct(s);return"mode="+f.mode+"\ncatX="+(int)s.catX+"\ncameraX/Y="+(int)f.cameraX+"/"+(int)f.elevation+"\nlookTargetX="+(int)f.lookX+"\nreason="+f.reason;}
+ public static String camera(WorldState s){CatCameraDirector.Frame f=CatCameraDirector.direct(s);double clear=s.girlPhysics==null?0:s.girlPhysics.groundClearanceM;return"mode="+f.mode+"\ncatX="+(int)s.catX+" haruX="+(int)s.haruX+"\ngirlClearanceM="+String.format(java.util.Locale.US,"%.2f",clear)+" girlLiftPx="+(int)f.girlScreenLiftPx+"\ncameraX/Y="+(int)f.cameraX+"/"+(int)f.elevation+" airFollowPx="+(int)f.airFollowPx+"\nlookTarget="+(int)f.lookX+","+(int)f.lookY+"\nreason="+f.reason;}
 }
