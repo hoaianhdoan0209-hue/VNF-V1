21929221dea036b7f680599b40758c47fee77161
hoaianhdoan0209-hue <hoaianhdoan0209@gmail.com>
2026-09-21 11:15:35 +0700

    Version persistent organism life-cycle state

--- app/src/main/java/com/aicharacter/v3/WorldState.java
+++ app/src/main/java/com/aicharacter/v3/WorldState.java
@@ -10,7 +10,7 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 
 public final class WorldState {
-    public static final int SAVE_VERSION = 37;
+    public static final int SAVE_VERSION = 38;
     public long createdAt;
     public long lastSavedAt;
     public long lastOpenedAt;
