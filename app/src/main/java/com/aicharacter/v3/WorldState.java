367f33f22b6bcbf00d991222866da63e7f58741b
hoaianhdoan0209-hue <hoaianhdoan0209@gmail.com>
2026-09-24 09:55:58 +0700

    divine: bump save schema for followers offerings and gifts

--- app/src/main/java/com/aicharacter/v3/WorldState.java
+++ app/src/main/java/com/aicharacter/v3/WorldState.java
@@ -10,7 +10,7 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 
 public final class WorldState {
-    public static final int SAVE_VERSION = 54;
+    public static final int SAVE_VERSION = 55;
     public long createdAt;
     public long lastSavedAt;
     public long lastOpenedAt;
