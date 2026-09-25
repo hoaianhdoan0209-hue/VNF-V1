ab03a4adb7b55f82e80b558bdf1984966eef06df
hoaianhdoan0209-hue <hoaianhdoan0209@gmail.com>
2026-09-25 17:30:11 +0700

    Fix proactive speech persistence string assertion

--- app/src/test/java/com/aicharacter/v3/VisualRegressionTest.java
+++ app/src/test/java/com/aicharacter/v3/VisualRegressionTest.java
@@ -223,7 +223,7 @@ public final class VisualRegressionTest{
   assertTrue(main.contains("setPositiveButton(\"Cho phép\""));
   assertFalse(main.contains("postDelayed(this::requestCoreRuntimePermissions"));
   assertTrue(world.contains("HaruProactiveSpeechState haruSpeech"));
-  assertTrue(world.contains("j.put(\\\"haruSpeech\\\",haruSpeech.toJson())"));
+   assertTrue(world.contains("j.put(\"haruSpeech\",haruSpeech.toJson())"));
   assertTrue(world.contains("HaruProactiveSpeechState.fromJson"));
  }
 
